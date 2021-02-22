c0320d093cd92d7b2fe96f121380a2e7157b2d1f
Joe Small <small.joseph@outlook.com>
2021-02-22 10:55:38 -0500

    changes to html structure

---
+++ script.js
@@ -0,0 +1,27 @@
+var timeleft = 60;
+// time starts once I click the start button
+// time is only reduced if an answer is answered incorrectly - otherwise it is not reduced (this makes no sense)
+// the game is over once timeLeft = 0 or all the questions are answered
+
+var firstQuestion = document.querySelector(".hidden")
+firstQuestion.addEventListener("click", )
+
+var userScore = 0;
+// userScore will start at 0. every question will be worth 20 points as a result if the user gets 5 questions right, they will score 100
+
+var start = document.querySelector("#start-button");
+
+start.addEventListener("click", beginGame);
+
+function beginGame() {
+    // call all of the functions in order
+
+}
+
+
+
+function questionOne() {
+
+}
+
+// have a prompt explaining the instructions for the quiz - maybe just write this out in html
